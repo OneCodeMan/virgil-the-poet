2b8a95646c93077ad0549d7045df4369ef2e0e11
Dave Gumba <dave_gumba@yahoo.com>
2018-06-14 20:22:03 -0400

    basic lighting implemented

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour {
+
+    public Material material;
+    private int counter;
+    private bool playerOnSurface = false;
+    private Color[] colors = new Color[] { Color.red, Color.green, Color.cyan, Color.magenta };
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerOnSurface)
+        {
+            material.SetColor("_EmissionColor", colors[counter]);
+            material.SetColor("_Color", colors[counter]);
+            counter++;
+
+            if (counter == colors.Length)
+            {
+                counter = 0;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.tag == "Player")
+        {
+            playerOnSurface = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.transform.tag == "Player")
+        {
+            playerOnSurface = false;
+        }
+    }
+}
---
+++ Assets/Scripts/TimedLightSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedLightSwitch : MonoBehaviour {
+
+    public Material material;
+    private int counter = 0;
+    private Color[] colors = new Color[] { Color.red, Color.green, Color.cyan, Color.magenta };
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("ChangeLight", 2.0f, 3.0f);
+	}
+
+    private void ChangeLight()
+    {
+
+        material.SetColor("_EmissionColor", colors[counter]);
+        material.SetColor("_Color", colors[counter]);
+        counter++;
+
+        if (counter == colors.Length)
+        {
+            counter = 0;
+        }
+
+    }
+}
